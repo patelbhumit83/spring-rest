3c64392fdc69de5de248c738017223f6cfdeee1e
Bhumit <patelbhumit83@gmail.com>
2021-06-16 16:44:38 -0700

    Request body mapping with method using Jackson

--- spring-rest/src/main/java/org/example/controller/EmployeeController.java
+++ spring-rest/src/main/java/org/example/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package org.example.controller;
 import org.example.entity.Employee;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,13 +24,13 @@ public class EmployeeController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/Employees")
-    public Employee create(Employee employee)
+    public Employee create(@RequestBody Employee employee)
     {
         return null;
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/Employees{id}")
-    public Employee update(@PathVariable("id") String id, Employee employee)
+    public Employee update(@PathVariable("id") String id, @RequestBody Employee employee)
     {
         return null;
     }
