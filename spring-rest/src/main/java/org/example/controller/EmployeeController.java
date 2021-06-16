122ca7f5e0275346c4d06ea969ec2645272831bc
Bhumit <patelbhumit83@gmail.com>
2021-06-16 16:52:41 -0700

    Combining controller and ResponseBody in RestController

--- spring-rest/src/main/java/org/example/controller/EmployeeController.java
+++ spring-rest/src/main/java/org/example/controller/EmployeeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
-@ResponseBody
+@RestController
 @RequestMapping(value = "Employees")
 public class EmployeeController {
 
