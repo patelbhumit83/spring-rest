9de6269f417ec97f96f71dfac7c767b1aa97c445
Bhumit <patelbhumit83@gmail.com>
2021-06-16 16:39:43 -0700

    Annotation for all methods

--- spring-rest/src/main/java/org/example/controller/EmployeeController.java
+++ spring-rest/src/main/java/org/example/controller/EmployeeController.java
@@ -2,33 +2,40 @@ package org.example.controller;
 
 import org.example.entity.Employee;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @Component
 public class EmployeeController {
 
+    @RequestMapping(method = RequestMethod.GET)
     public List<Employee> findall(){
         return null;
     }
 
+    @RequestMapping(method = RequestMethod.GET)
     public Employee findone(String id)
     {
         return null;
     }
 
+    @RequestMapping(method = RequestMethod.POST)
     public Employee create(Employee employee)
     {
         return null;
     }
 
+    @RequestMapping(method = RequestMethod.PUT)
     public Employee update(String id, Employee employee)
     {
         return null;
     }
 
+    @RequestMapping(method = RequestMethod.DELETE)
     public void delete(String id)
     {
-        
+
     }
 }
