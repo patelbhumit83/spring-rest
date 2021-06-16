93d28fa96e011962cc7b484cc5e67118984fca4e
Bhumit <patelbhumit83@gmail.com>
2021-06-16 16:37:47 -0700

    EmployeeController is added in Controller package

---
+++ spring-rest/src/main/java/org/example/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package org.example.controller;
+
+import org.example.entity.Employee;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class EmployeeController {
+
+    public List<Employee> findall(){
+        return null;
+    }
+
+    public Employee findone(String id)
+    {
+        return null;
+    }
+
+    public Employee create(Employee employee)
+    {
+        return null;
+    }
+
+    public Employee update(String id, Employee employee)
+    {
+        return null;
+    }
+
+    public void delete(String id)
+    {
+        
+    }
+}
